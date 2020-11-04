cc5df0d159d70a60ca189898e9f42bae11955d0f
illianes <pierremariotte98@mail.com>
2020-11-04 18:43:04 +0100

    Player + Mur + Sol + Mouvement + Borne + Tag

---
+++ rookieTeamGameProject/Assets/Asset/Script/Mouvement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Time.deltaTime * speed, 0, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(-Time.deltaTime * speed, 0, 0);
+        }
+    }
+
+
+}
