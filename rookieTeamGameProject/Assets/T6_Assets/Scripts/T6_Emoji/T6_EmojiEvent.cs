5a0b143912fc27530f2c9220d63b2f803b7deea3
Noé <noeguillaumin@gmail.com>
2020-11-06 11:06:53 +0100

    delete T6
    delete T6 parce que F U C K

--- rookieTeamGameProject/Assets/T6_Assets/Scripts/T6_Emoji/T6_EmojiEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public static class T6_EmojiEvent
-{
-    public static UnityEvent hitEmojiEvent = new UnityEvent();
-}
