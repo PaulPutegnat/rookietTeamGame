49b311c0227998026632a5615b422784f4cb8ed4
illianes <pierremariotte98@mail.com>
2020-11-06 12:31:49 +0100

    interaction borne

--- rookieTeamGameProject/Assets/SalleAsset/Script/Mouvement.cs
+++ rookieTeamGameProject/Assets/SalleAsset/Script/Mouvement.cs
@@ -10,7 +10,16 @@ public class Mouvement : MonoBehaviour
 
     private SpriteRenderer SpriteR;
 
+    public bool isInTeam0 = false;
+    public bool isInTeam1 = false;
+    public bool isInTeam2 = false;
     public bool isInTeam3 = false;
+    public bool isInTeam4 = false;
+    public bool isInTeam6 = false;
+    public bool isInTeam7 = false;
+    public bool isInTeam8 = false;
+    public bool isInTeam9 = false;
+    public bool isInTeam10 = false;
 
     [SerializeField] private float speed;
 
@@ -46,7 +55,52 @@ public class Mouvement : MonoBehaviour
 
     private void Update()
     {
+        if (isInTeam0 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam1 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam2 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
         if (isInTeam3 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam4 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam6 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam7 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam8 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam9 && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Smileyxique");
+        }
+
+        if (isInTeam10 && Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene("Smileyxique");
         }
@@ -59,18 +113,90 @@ public class Mouvement : MonoBehaviour
             SceneManager.LoadScene("T1Menu");
         }*/
 
+        if (collision.gameObject.CompareTag("Team0"))
+        {
+            isInTeam0 = true;
+        }
+        if (collision.gameObject.CompareTag("Team1"))
+        {
+            isInTeam1 = true;
+        }
+        if (collision.gameObject.CompareTag("Team2"))
+        {
+            isInTeam2 = true;
+        }
         if (collision.gameObject.CompareTag("Team3"))
         {
             isInTeam3 = true;
+        }
+        if (collision.gameObject.CompareTag("Team4"))
+        {
+            isInTeam4 = true;
+        }
+        if (collision.gameObject.CompareTag("Team6"))
+        {
+            isInTeam6 = true;
+        }
+        if (collision.gameObject.CompareTag("Team7"))
+        {
+            isInTeam7 = true;
+        }
+        if (collision.gameObject.CompareTag("Team8"))
+        {
+            isInTeam8 = true;
+        }
+        if (collision.gameObject.CompareTag("Team9"))
+        {
+            isInTeam9 = true;
+        }
+        if (collision.gameObject.CompareTag("Team10"))
+        {
+            isInTeam10 = true;
         }
     }
 
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("Team0"))
+        {
+            isInTeam0 = false;
+        }
+        if (collision.gameObject.CompareTag("Team1"))
+        {
+            isInTeam1 = false;
+        }
+        if (collision.gameObject.CompareTag("Team2"))
+        {
+            isInTeam2 = false;
+        }
         if (collision.gameObject.CompareTag("Team3"))
         {
             isInTeam3 = false;          
+        }
+        if (collision.gameObject.CompareTag("Team4"))
+        {
+            isInTeam4 = false;
+        }
+        if (collision.gameObject.CompareTag("Team6"))
+        {
+            isInTeam6 = false;
+        }
+        if (collision.gameObject.CompareTag("Team7"))
+        {
+            isInTeam7 = false;
+        }
+        if (collision.gameObject.CompareTag("Team8"))
+        {
+            isInTeam8 = false;
+        }
+        if (collision.gameObject.CompareTag("Team9"))
+        {
+            isInTeam9 = false;
+        }
+        if (collision.gameObject.CompareTag("Team10"))
+        {
+            isInTeam10 = false;
         }
     }
 }
