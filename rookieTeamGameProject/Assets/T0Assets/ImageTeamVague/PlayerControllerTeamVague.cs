e814533f583f4d417e32ec74cd7b0f053ec3da68
Djarlem <49781705+Djarlem@users.noreply.github.com>
2020-11-06 12:17:40 +0100

    Ajout T0 final version

---
+++ rookieTeamGameProject/Assets/T0Assets/ImageTeamVague/PlayerControllerTeamVague.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerTeamVague : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> tpplayer;
+    public int nbr;
+    public int nbrmax;
+    public ControlToucheTeamVague control;
+    public bool fin;
+   
+    void Start()
+    {
+        Move(nbr);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(control.down))
+        {
+            nbr += 1;
+            Move(nbr);
+        }
+        else if (Input.GetKeyDown(control.up))
+        {
+            nbr -= 1;
+            Move(nbr);
+
+        }
+    }
+
+
+    public void Move(int count)
+    {
+        if(count<0)
+        {
+            nbr += 1;
+        }else if(count>nbrmax)
+        {
+            nbr -= 1;
+        }
+        transform.position = tpplayer[nbr].position;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("DeathTrigger"))
+        {
+            if (fin)
+            {
+                SceneManager.LoadScene("ScoringTeamVague");
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+}
---
+++ rookieTeamGameProject/Assets/T0Assets/ImageTeamVague/ScoreFinalTeamVague.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreFinalTeamVague : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Audioo;
+    public TextMeshProUGUI text;
+    void Start()
+    {
+        Audioo = GameObject.Find("SoundManagerTeamVague");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Audioo!=null)
+        {
+
+        text.text = "Votre Score: " + Audioo.GetComponent<AudioManagerTeamVague>().Score.ToString();
+        }
+    }
+}
